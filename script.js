1b14c229b7e74e5b229d61380adecfec35d3e524
Seungyeon0321 <71824405+Seungyeon0321@users.noreply.github.com>
2023-04-06 22:50:24 -0700

    add clearing opacity

--- script.js
+++ script.js
@@ -9,8 +9,13 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const nav__links = document.querySelector('.nav__links');
 const nav__link = document.querySelectorAll('.nav__link');
+const sections = document.querySelectorAll('.section');
 const section1 = document.querySelector('#section--1');
+const section2 = document.querySelector('#section--2');
+const section3 = document.querySelector('#section--3');
+const features__img = document.querySelectorAll('.features__img');
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -39,17 +44,16 @@ document.addEventListener('keydown', e => {
 });
 
 // Smooth하게 내려가기 (이거 event delegation으로 한 번 해보기)
-nav__link.forEach(link =>
-  link.addEventListener('click', function (e) {
-    e.preventDefault();
-    console.log(e.target);
-    let targetID = this.getAttribute('href');
+nav__links.addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log(e.target);
 
-    document.querySelector(targetID).scrollIntoView({
-      behavior: 'smooth',
-    });
-  })
-);
+  //matching이란 의미는 그 target은 딱 정하는 거, if we do not have this matching, this handler will fire when we click its' parents element
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
 
 //샘은 target을 이용해서 해결, 그리고 Matching strategy라는 것을 이용,
 // document.querySelector('.nav__links').addEventListener('click', function (e) {
@@ -79,7 +83,9 @@ const navHeight = nav.getBoundingClientRect();
 
 const callback = function (e, o) {
   e.forEach(e => {
-    !e.isIntersecting ? nav.classList.add('sticky') : ''; //분명히 option에 맞는 조건에 들어왔는데 왜 isIntersecting value는 false인가?
+    !e.isIntersecting
+      ? nav.classList.add('sticky')
+      : nav.classList.remove('sticky'); //분명히 option에 맞는 조건에 들어왔는데 왜 isIntersecting value는 false인가? 조건이 맞았을 때 event가 동작하는 거고 실제로 intersecting만 봤을 때는 더이상 intersecting하고 있지 않기 때문에 이는 false이다
   });
 };
 
@@ -93,17 +99,58 @@ const observer = new IntersectionObserver(callback, option);
 
 observer.observe(header); //header가 viewport에서 10 거의 없어질 때 딱 nav height정도가 됐을 때 nav가 sticky position을 얻도록 함.
 
-// const option = {
-//   // root: section1,
-//   rootMargin: '10px',
-//   threshold: 1.0,
-// };
+//opacity clearing
+sections.forEach(v => v.classList.add('section--hidden'));
 
-// const callback = (e, o) => {
-//   let entry = e[0];
-//   console.log(entry);
-//   if (entry.isIntersecting == true) console.log('hello world!');
-// };
+const clearOpacity = function (e, o) {
+  e.forEach(e => {
+    e.isIntersecting ? section1.classList.remove('section--hidden') : '';
+    e.isIntersecting
+      ? features__img.forEach(v => v.classList.remove('lazy-img'))
+      : '';
+  });
+};
+
+const observerForOpacity = new IntersectionObserver(clearOpacity, {
+  root: null,
+  threshold: 0.4,
+});
+
+observerForOpacity.observe(section1);
+
+///////////////////2///////////////////
+
+const clearOpacity2 = function (e, o) {
+  e.forEach(e => {
+    e.isIntersecting ? section2.classList.remove('section--hidden') : '';
+  });
+};
+
+const observerForOpacity2 = new IntersectionObserver(clearOpacity2, {
+  root: null,
+  threshold: 0.4,
+});
+
+observerForOpacity2.observe(section2);
+
+///////////////////3//////////////////
+
+const clearOpacity3 = function (e, o) {
+  e.forEach(e => {
+    e.isIntersecting ? section3.classList.remove('section--hidden') : '';
+  });
+};
+
+const observerForOpacity3 = new IntersectionObserver(clearOpacity3, {
+  root: null,
+  threshold: 0.4,
+});
+
+observerForOpacity3.observe(section3);
+
+//I don't know how it can be seperated
+
+//Do I have to make several observe to make the image clear?
 
 // const observer = new IntersectionObserver(callback, option);
 // // 1. 새로운 객체를 만들어야 함
